443f07fd13e82957242a230fe2ef472b6f482e0e
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-02-18 22:17:46 +0530

    Create Maximum_Consecutive_One..java

---
+++ ARRAYS/Maximum_Consecutive_One..java
@@ -0,0 +1,22 @@
+package ARRAYS;
+
+public class Maximum_Consecutive_One {
+    static int consecutive(int arr[]){
+        int max =0;
+        int count  = 0;
+        for (int i =0;i< arr.length;i++){
+            if (arr[i] == 1){
+                count++;
+            }else {
+                count  =0;
+            }
+            max= Math.max(max,count);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,1,0,1,1,1,0};
+        System.out.println(consecutive(arr));
+    }
+}
