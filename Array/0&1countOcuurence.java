99cbddc1e60123bfb1c73f957e9aee775b4b08d9
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-02-20 16:19:46 +0530

    Create 0&1countOcuurence.java

---
+++ Array/0&1countOcuurence.java
@@ -0,0 +1,49 @@
+package ARRAYS;
+/*
+https://www.geeksforgeeks.org/batch/gfg-160-problems/track/sorting-gfg-160/problem/sort-an-array-of-0s-1s-and-2s4231
+Sort 0s, 1s and 2s
+Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+You need to solve this problem without utilizing the built-in sort function.
+Examples:
+Input: arr[] = [0, 1, 2, 0, 1, 2]
+Output: [0, 0, 1, 1, 2, 2]
+Explanation: 0s 1s and 2s are segregated into ascending order.
+ */
+public class countOcuurence {
+    static void swap(int arr[],int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void sort(int arr[]){
+        int mid =0;
+        int low  =0 ;
+        int n = arr.length;
+        int h = n-1;
+        while (mid<=h){
+            if (arr[mid]==0){
+                swap(arr,mid,low );
+                low++;
+                mid++;
+            }
+            else if (arr[mid] == 1)
+            {
+                mid++;
+            }
+          else   {
+              swap(arr,mid,h);
+              h--;
+
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1,0,2 };
+        sort(arr);
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
