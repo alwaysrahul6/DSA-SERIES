0023e31a156fd13fa0d76879fe0ce30badf37770
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-03-30 19:05:59 +0530

    Create Decreasing_Binary_Search.java

---
+++ BINARY_SEARCH/Decreasing_Binary_Search.java
@@ -0,0 +1,25 @@
+package BINARY_SEARCH;
+
+public class Decreasing_Binary_Search {
+    static int binary(int arr[],int ele){
+        int n = arr.length;
+        int i =0;
+        int j = n-1;
+        while (i<=j){
+            int mid = i+j-i/2;
+            if (ele == arr[mid]){
+                return  mid;
+            } else if (ele<arr[mid]) {
+                i =  mid +1;
+            }else
+                j = mid -1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {20,19,18,17,15,14,12,11,5};
+        int ele = 5;
+        System.out.println(binary(arr,ele));
+    }
+}
