2ddb9166aabf9b78a9e6e376576b629ce8ee7664
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-04-04 14:16:11 +0530

    Create Minimum_Jump.java
    approch: sum the index value and index and store the maximum

---
+++ GREEDY/Minimum_Jump.java
@@ -0,0 +1,27 @@
+package GREEDY;
+
+import java.util.Scanner;
+
+public class Minimum_Jump {
+    static boolean solve(int arr[]){
+        int n = arr.length;
+        int maxindex= 0;
+        for (int i =0;i<n;i++){
+            if (i>maxindex)return false;
+            maxindex = Math.max(maxindex,arr[i]+i);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Size:");
+        int n = scanner.nextInt();
+        System.out.println("Enter Element");
+        int arr[] = new int[n];
+        for (int i =0;i<n;i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(solve(arr));
+    }
+}
