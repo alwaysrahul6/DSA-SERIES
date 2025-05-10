988ada9e2bb4d1c5ff7d22131381f942b008cf3d
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-05-10 21:50:03 +0530

    Create test .java

---
+++ OOPS/Inheritance/test .java
@@ -0,0 +1,11 @@
+package OOPS.Inheritance;
+
+public class test {
+    public static void main(String[] args) {
+        cat caat  =  new cat();
+        caat.name = "mauli";
+        System.out.println(caat.name);
+        caat.age = 20;
+        System.out.println(caat.age);
+    }
+}
