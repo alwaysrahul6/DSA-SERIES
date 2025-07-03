ec71dd25173e00463f8f722240fc3c4ec256ddf0
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-07-03 19:40:59 +0530

    Update Method_Basic.java

--- FUNCTION/Method_Basic.java
+++ FUNCTION/Method_Basic.java
@@ -2,7 +2,7 @@ package FUNCTION;
 
 public class Method_Basic {
     public static void naam(){
-        System.out.println("Hi Good Evening");
+        System.out.println(".    Hi Good Evening");
         for (int i =1;i<=20;i++){
 
             System.out.println(i);
