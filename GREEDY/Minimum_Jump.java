a1c36d19f1d6e79d71460ab36d0544eea396a84b
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-07-04 22:27:17 +0530

    Update Minimum_Jump.java

--- GREEDY/Minimum_Jump.java
+++ GREEDY/Minimum_Jump.java
@@ -6,7 +6,7 @@ public class Minimum_Jump {
     static boolean solve(int arr[]){
         int n = arr.length;
         int maxindex= 0;
-        for (int i =0;i<n;i++){
+        for (int i = 0;i< n;i++){
             if (i>maxindex)return false;
             maxindex = Math.max(maxindex,arr[i] + i);
         }
