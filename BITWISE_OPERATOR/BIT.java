cfc3c3f2b181849805639c1e90777ae4b0ff48a6
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-07-09 15:06:39 +0530

    Add files via upload

---
+++ BITWISE_OPERATOR/BIT.java
@@ -0,0 +1,19 @@
+package BITWISE_OPERATOR;
+
+public class BIT {
+    public static void main(String[] args) {
+        int a  = 3;
+        int b = 5;
+        System.out.println(a^b);
+        //        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(a^b));
+
+
+//        left shift
+
+        int l = 5;
+        int m =  l << 2;
+        System.out.println(m);
+
+    }
+}
---
+++ BITWISE_OPERATOR/BIT_UNSIGNED.java
@@ -0,0 +1,11 @@
+package BITWISE_OPERATOR;
+
+public class BIT_UNSIGNED {
+    public static void main(String[] args) {
+        int a  = -8;
+        System.out.println(Integer.toBinaryString(a));
+        int result = a >> 2;
+        System.out.println(result);
+        System.out.println(Integer.toBinaryString(result));
+    }
+}
