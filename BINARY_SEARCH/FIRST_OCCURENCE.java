88f22f40365f5e17717ba08854389cae348ba2ef
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-09-12 22:36:37 +0530

    Update FIRST_OCCURENCE.java

--- BINARY_SEARCH/FIRST_OCCURENCE.java
+++ BINARY_SEARCH/FIRST_OCCURENCE.java
@@ -6,7 +6,7 @@ Output: 3
 * */
 import java.util.Scanner;
 
-public class FIRST_OCCURENCE {
+public class FIRST_OCCURENCE {   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Size:");
