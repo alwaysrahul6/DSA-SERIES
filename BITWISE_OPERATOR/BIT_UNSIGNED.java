5ebf1b8f629046b3fbaba509f6f8a10e455c6d5d
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-11-29 22:38:18 +0530

    Created BIT_UNSIGNED.java

--- BITWISE_OPERATOR/BIT_UNSIGNED.java
+++ BITWISE_OPERATOR/BIT_UNSIGNED.java
@@ -2,7 +2,7 @@ package BITWISE_OPERATOR;
 
 public class BIT_UNSIGNED {
     public static void main(String[] args) {
-        int a  = -8;
+        int a  = -8;  
         System.out.println(Integer.toBinaryString(a));
         int result = a >> 2;
         System.out.println(result);
